4685744bff292a1be982919d18e3a73e0e08c6c7
Rene van der Ark <r.van.der.ark@gmail.com>
2017-10-05 13:09:04 +0200

    Distributes version 1.2.

--- sample/install-sample.sh
+++ sample/install-sample.sh
@@ -8,8 +8,11 @@ mkdir -p output/in
 mkdir output/rejected
 mkdir output/processing
 
-java -jar oai-pmh-bulk-downloader-latest.jar server example.yaml
+java -jar oai-pmh-bulk-downloader-latest.jar server example.yaml > application.log &
+SERVER_PID=$!
 
 curl -XPOST http://localhost:18081/tasks/create-database-schema
 curl -XPOST -F 'file=@anp.xsl' http://localhost:18080/stylesheets
 curl -XPOST -H 'Accept: application/json' -H 'Content-type: application/json' -d@anp.json  http://localhost:18080/repositories/
+
+echo "run 'kill $SERVER_PID' to stop the application"
